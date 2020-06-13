1a7a70be38e67dc7968e8042a8e0dc2759b02092
unknown <gauthamsrinivas95@gmail.com>
2020-06-13 17:50:06 +0530

    changes7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { CountdownComponent } from './countdown/countdown.component';
 import { CountdownModule } from "ng2-date-countdown";
 import { MapComponent } from './map/map.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CountdownComponent,
-    MapComponent
+    MapComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -13,9 +13,13 @@ export class MapComponent implements OnInit {
   countdownShow: boolean = false;
   mapShow: boolean = true;
   titleShow: boolean = false;
+  audio = new Audio();
+  audio1 = new Audio();
   constructor(private router:Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+  }
 
   open() {
     if(this.openmap){
@@ -23,13 +27,25 @@ export class MapComponent implements OnInit {
     }else{
       this.openmap = true;
     }
+    this.play();
     //this.openmap = true;
-    //setTimeout(() => this.router.navigateByUrl('/countdown'),20000);
+    setTimeout(() => this.close(),19000);
   }
 
   close() {
     this.openmap == false;
     this.fadeout = true;
-    setTimeout(() => {this.countdownShow = false, this.mapShow=false, this.titleShow = true},8000);
+    setTimeout(() => {this.countdownShow = false, this.mapShow=false, this.titleShow = true, this.audio.pause(), this.hbdPlay()},5000);
+  }
+
+   play() {
+    this.audio.src = "assets/music/harry_potter.mp3";
+    this.audio.load();
+    this.audio.play();
+  }
+  hbdPlay(){
+    this.audio1.src = "assets/music/DJ_BoBo_Happy_Birthday.mp3";
+    this.audio1.load();
+    this.audio1.play();
   }
 }
